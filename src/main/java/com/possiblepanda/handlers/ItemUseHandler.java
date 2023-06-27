4caf45bca834850466d2e092018d69e4d2c24c6b
Possible Panda <possiblepanda14@gmail.com>
2023-06-27 17:05:09 -0400

    Made spawn egg work

--- src/main/java/com/possiblepanda/handlers/ItemUseHandler.java
+++ src/main/java/com/possiblepanda/handlers/ItemUseHandler.java
@@ -185,9 +185,9 @@ public class ItemUseHandler implements Listener {
     @EventHandler
     public void onSpawnEggUse(PlayerInteractEvent event) {
         event.getPlayer().sendMessage("hi");
-        event.getPlayer().sendMessage(event.getItem().getItemMeta().getLore().toString());
-        event.getPlayer().sendMessage(ItemHandler.speedZombieSpawnEgg.getItemMeta().getLore().toString());
-        if(event.getItem().getItemMeta().getLore() == ItemHandler.speedZombieSpawnEgg.getItemMeta().getLore()) {
+        event.getPlayer().sendMessage(event.getItem().getItemMeta().toString());
+        event.getPlayer().sendMessage(ItemHandler.speedZombieSpawnEgg.getItemMeta().toString());
+        if(event.getItem().getItemMeta().equals(ItemHandler.speedZombieSpawnEgg.getItemMeta())) {
             event.getPlayer().sendMessage("hi2 :O");
             event.setCancelled(true);
             Location loc;
